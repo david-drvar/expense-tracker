3d54fc550a04943169290018241f8a0e79bbf2d9
David Drvar <david.drvar.bogdanovic@gmail.com>
2020-03-29 23:33:04 +0200

    Setting the basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:expense_tracker/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +17,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'new shoes', amount: 6999, date: DateTime.now()),
+    Transaction(id: 't2', title: 'new pc', amount: 6999, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,18 +30,84 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
-              child: Card( // if you want to increase Card's width you can do it by wraping
-                          // Text with a Container or wraping whole Card in a Container
+              child: Card(
+                // if you want to increase Card's width you can do it by wraping
+                // Text with a Container or wraping whole Card in a Container
                 color: Colors.blue,
                 child: Text('CHART'),
-                elevation: 50,
+                elevation: 5,
               ),
             ),
             Card(
-              child: Text('LIST OF TX'),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    FlatButton(
+                      child: Text('Add transaction'),
+                      onPressed: () {},
+                      textColor: Colors.purple,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        //color: Colors.purple,
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17),
+                          ),
+                          Text(
+                            DateFormat().format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             )
           ],
         ));
